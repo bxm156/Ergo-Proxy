a24e64de0904e142e9463d4a7b87afeb266f79d6
Bryan Marty <bxm156@case.edu>
2012-10-27 03:22:54 -0400

    per byte basis

--- src/com/bryanmarty/ergoproxy/SocketTask.java
+++ src/com/bryanmarty/ergoproxy/SocketTask.java
@@ -25,7 +25,7 @@ public class SocketTask implements Runnable {
 		}
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket_.getInputStream()));
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket_.getOutputStream()));
+			//BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket_.getOutputStream()));
 			
 			StringBuffer dataBuffer = new StringBuffer();
 			String line;
@@ -37,11 +37,9 @@ public class SocketTask implements Runnable {
 				dataBuffer.append(line);
 				dataBuffer.append(NEW_LINE);
 			}
-			//reader.close();
 			socket_.shutdownInput();
 			
 			String data = dataBuffer.toString();
-			//System.out.println(data);
 			HttpRequest request = HttpParser.parse(data);
 			/*
 			//First look for the file
@@ -56,41 +54,25 @@ public class SocketTask implements Runnable {
 			Socket sRequest = new Socket(request.getHost(), 80);
 			
 			BufferedWriter toServer = new BufferedWriter(new OutputStreamWriter(sRequest.getOutputStream()));
-			//BufferedReader fromServer = new BufferedReader(new InputStreamReader(sRequest.getInputStream()));
-			BufferedReader fromServer = new BufferedReader(new InputStreamReader(sRequest.getInputStream()));
 			HttpIO.send(data, toServer);
+			sRequest.shutdownOutput();
 			
-			StringBuffer sb = new StringBuffer();
-			char[] buffer = new char[1024];
 			int d;
-			long startTime = System.currentTimeMillis();
-			while(!sRequest.isInputShutdown()) {
-				if(startTime - System.currentTimeMillis() > 7000) {
+			byte[] buffer = new byte[4098];
+			while(true) {
+				d = sRequest.getInputStream().read(buffer,0,buffer.length);
+				if(d == -1) {
 					break;
 				}
-				if(fromServer.ready()) {
-					while((d = fromServer.read(buffer,0,buffer.length)) != -1) {
-						String s = new String(buffer, 0, d);
-						sb.append(s);
-						if(!fromServer.ready()) {
-							break;
-						}
-					}
-					if(sb.length() > 0 && HttpParser.validate(sb.toString())) {
-						break;
-					}
-				}				
-			}
-			writer.write(sb.toString());
-			writer.flush();
-			
-			//fromServer.close();
-			sRequest.shutdownInput();
-			socket_.shutdownOutput();
+				System.out.println(new String(buffer,0,d));
+				socket_.getOutputStream().write(buffer,0,d);
+				socket_.getOutputStream().flush();
 
-			
+			}
 			sRequest.close();
-			socket_.close();
+			socket_.getOutputStream().flush();
+			socket_.shutdownOutput();
+			toServer.close();
 			
 			
 			
