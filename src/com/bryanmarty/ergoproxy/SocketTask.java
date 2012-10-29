8b33f382390443cbfe5305b4b2152891d8530d8f
Bryan Marty <bxm156@case.edu>
2012-10-28 23:01:09 -0400

    better, still some issues?

--- src/com/bryanmarty/ergoproxy/SocketTask.java
+++ src/com/bryanmarty/ergoproxy/SocketTask.java
@@ -7,16 +7,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class SocketTask implements Runnable {
 
 	private static final String NEW_LINE = "\r\n";
 	private static final DataCache DATA_CACHE = DataCache.getInstance();
 	private final Socket socket_;
+	private BlockingQueue<Runnable> blockingQueue_;
+	private ThreadPoolExecutor workerPool_;
+	private LinkedList<Future<ByteBuffer>> futureList = new LinkedList<Future<ByteBuffer>>();
 	
 	public SocketTask(Socket clientSocket) {
 		socket_ = clientSocket;
+		blockingQueue_ = new LinkedBlockingQueue<Runnable>();
+		workerPool_ = new ThreadPoolExecutor(1, 10, 3L, TimeUnit.SECONDS, blockingQueue_);
 	}
 
 	@Override
@@ -31,52 +44,63 @@ public class SocketTask implements Runnable {
 			
 			String line = null;
 			while(!socket_.isInputShutdown()) {
-				
-				StringBuffer dataBuffer = new StringBuffer();
-				
-				
-				Scanner sc = new Scanner(socket_.getInputStream());
-				sc.useDelimiter("\r\n");
-				while (sc.hasNextLine()) {
-					line = sc.nextLine();
-					if(line.isEmpty()) {
-						dataBuffer.append(NEW_LINE);
-						break;
-					}
-					dataBuffer.append(line);
-					dataBuffer.append(NEW_LINE);
-				}
 
-				if(line == null) {
-					socket_.shutdownOutput();
-					break;
-				}
-				System.out.println("Finished Reading");
-				System.out.println("Count: " + count);
-				count++;
-				String data = dataBuffer.toString();
-				HttpRequest request = HttpParser.parse(data);
-				Socket sRequest = new Socket(request.getHost(), request.getPort());
-				BufferedWriter toServer = new BufferedWriter(new OutputStreamWriter(sRequest.getOutputStream()));
-				HttpIO.send(data, toServer);
-				sRequest.shutdownOutput();
-				int d;
-				byte[] buffer = new byte[4098];
-				while(!socket_.isOutputShutdown() && !sRequest.isInputShutdown()) {
-					d = sRequest.getInputStream().read(buffer,0,buffer.length);
-					if(d == -1) {
-						sRequest.close();
+				while(reader.ready()) {
+					StringBuffer dataBuffer = new StringBuffer();
+					Scanner sc = new Scanner(socket_.getInputStream());
+					
+					sc.useDelimiter("\r\n");
+					while (sc.hasNextLine()) {
+						line = sc.nextLine();
+						if(line.isEmpty()) {
+							dataBuffer.append(NEW_LINE);
+							break;
+						}
+						dataBuffer.append(line);
+						dataBuffer.append(NEW_LINE);
+					}
+	
+					if(line == null) {
+						socket_.shutdownOutput();
 						break;
 					}
-					socket_.getOutputStream().write(buffer, 0, d);
-					socket_.getOutputStream().flush();
+					System.out.println("Finished Reading");
+					System.out.println("Count: " + count);
+					count++;
+					String data = dataBuffer.toString();
+					if(data.length() <= 0) {
+						continue;
+					}
+					HttpRequest request = HttpParser.parse(data);
 					
+					DownloadTask d = new DownloadTask(request.getHost(),request.getPort(),data);
+					Future<ByteBuffer> futureResult = workerPool_.submit(d);
+					futureList.add(futureResult);
 				}
+				
+				for(Future<ByteBuffer> future : futureList) {
+					try {
+						ByteBuffer download = future.get();
+						socket_.getOutputStream().write(download.array(), 0, download.position());
+						socket_.getOutputStream().flush();
+						String output = new String(download.array(),0,download.position());
+						System.out.println(output);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (ExecutionException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				
+				
 				System.out.println("Server responded and terminated");
 				if(!reader.ready()) {
 					break;
 				}
-			}	
+			}
+			
+			workerPool_.shutdownNow();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
