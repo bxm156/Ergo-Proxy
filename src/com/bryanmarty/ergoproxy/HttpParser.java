7386b970b19d49a94ae45ba3f02f2aba1a475153
Bryan Marty <bxm156@case.edu>
2012-11-04 16:36:57 -0500

    small adjustments

--- src/com/bryanmarty/ergoproxy/HttpParser.java
+++ src/com/bryanmarty/ergoproxy/HttpParser.java
@@ -5,20 +5,35 @@ import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class HttpParser.
+ */
 public class HttpParser {
 	
 	//Request
+	/** The Constant pGetVersion. */
 	public static final Pattern pGetVersion = Pattern.compile("^(GET|POST|HEAD)[\\W]{1}(.*)[\\W]{1}HTTP/(.*)$",Pattern.MULTILINE);
+	
+	/** The Constant pHost. */
 	public static final Pattern pHost = Pattern.compile("^Host:[\\W]+(.*)$",Pattern.MULTILINE);
+	
+	
+	/**
+	 * Parses the HTTP Request for specific values.
+	 *
+	 * @param info the HTTP request string
+	 * @return the HttpRequest object for that request.
+	 */
 	public static HttpRequest parse(String info) {
 		HttpRequest request = new HttpRequest();
-
+		URL url = null;
 		Matcher m = pGetVersion.matcher(info);
 		if(m.find()) {
 			try{
 				//Try and get the relative url
 				String file = m.group(2).trim();
-				URL url = new URL(file);
+				url = new URL(file);
 				info = m.replaceFirst(Matcher.quoteReplacement(m.group(1).trim() + " " + url.getFile() + " HTTP/" + m.group(3)));
 			} catch (MalformedURLException mue) {
 				// We will use the original request
@@ -37,6 +52,11 @@ public class HttpParser {
 				request.setHost(pieces[0]);
 				request.setPort(Integer.valueOf(pieces[1]));
 			}
+		}
+		
+		//Just in case the host is blank, but we have the requested file...
+		if ((request.getHost() == null || request.getHost().isEmpty()) && url != null) {
+			request.setHost(url.getHost());
 		}
 		
 		request.setRequest(info);
--- src/com/bryanmarty/ergoproxy/SocketTask.java
+++ src/com/bryanmarty/ergoproxy/SocketTask.java
@@ -22,12 +22,14 @@ public class SocketTask implements Runnable {
 
 	@Override
 	public void run() {
+		//We were given a null client, so ignore and quit the thread.
 		if(client_ == null) {
 			return;
 		}
+		
+		//Setup the input and output stream to the client.
 		BufferedReader clientInput = null;
 		OutputStream cos = null;
-		//Setup the input and output stream to the client.
 		try {
 			clientInput = new BufferedReader(new InputStreamReader(client_.getInputStream(),"US-ASCII"));;
 			cos = client_.getOutputStream();
@@ -36,11 +38,15 @@ public class SocketTask implements Runnable {
 			closeClient();
 			return;
 		}
+		//Now we read data from the client and launch download threads...
 		try {
 			StringBuffer bufferData = new StringBuffer();
+			/*We set to the client timeout. This allows us to to check for various
+			conditions while reading. Timing out does not imply that we will close the socket,
+			or that we are done with the socket */
+			client_.setSoTimeout(1000);
 			while(client_.isConnected()) {
 				String line = null;
-				client_.setSoTimeout(100);
 				//Read HTTP Requests
 				try {
 					while((line = clientInput.readLine()) != null) {
@@ -52,25 +58,35 @@ public class SocketTask implements Runnable {
 						bufferData.append(line + NEW_LINE);
 					}
 				} catch (SocketTimeoutException e) {
+					/* Thus just means we have not had a new request yet, we will check the current
+					 * status of things and then keep waiting for more requests from the browser.
+					 */
 					if(!client_.isConnected() || client_.isOutputShutdown() || !download_.isAlive()) {
-					
+						//We have may have downloaded the last item. The client may have left the connection to our proxy open,
+						//but has not given us any new requests, there are no connections to remote servers at this time.
 						break;
 						
 					}
+					//We will continue accepting more data.
 					continue;
 					
 				}
 				
+				//The client has closed the socket...
 				if(line == null) {
 					//End the task
 					break;
 				}
 				
+				//Take the HTTP Request, create a string of it, and clear the buffer.
 				String data = bufferData.toString();
 				bufferData.setLength(0);
 				if(data.isEmpty()) {
+					//We dont want to act upon empty data
 					continue;
 				}
+				
+				//Parse the request
 				HttpRequest request = HttpParser.parse(data);
 				
 				//EECS 425 DNS Cache
@@ -79,16 +95,20 @@ public class SocketTask implements Runnable {
 					//Try and retrieve the IP Address
 					hostIP = DNS_CACHE.retrieve(request.getHost());
 				} catch (NullPointerException npe) {
-					continue; //If the host was empty, ignore the request.
+					continue;
 				}
 				
-				//Before we open a new request to a server, close the existing
-				//connection to the previous remote server
+				/* Before we open a new request to a server, close the existing
+				 * connection to the previous remote server, which we are expecting
+				 * no new data.
+				 */
 				closeDownloadThread();
 				
 				//Connect to the server
 				server_ = new Socket(hostIP,request.getPort());
-				server_.setSoTimeout(5000);
+				server_.setSoTimeout(5000); /* In case of the last item to be downloaded, we may never receive another HTTP request
+				from the client, and the client may leave our connection open, so we have this to terminate the connection to server.
+				This value is not a timeout of the entire socket, but the amount of time that we will wait for new data to be read. */
 				if(server_.isConnected()) {
 					//Start the thread to transfer data to the client
 					download_ = new DownloadThread(cos,server_,request);
